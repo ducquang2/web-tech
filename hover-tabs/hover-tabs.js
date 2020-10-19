98fb4ce7ceaf53d0347d64f69277b420ee56538e
ducquang2 <quangbilun@gmail.com>
2020-10-19 20:57:56 +0700

    hover-tabs

---
+++ hover-tabs/hover-tabs.js
@@ -0,0 +1,10 @@
+function sections(evt, Name) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].getElementsByClassName.display = "none";
+    }
+}
