d4df8fe8161b924fc2e9765ecd15a6905e523ca8
ducquang2 <quangbilun@gmail.com>
2020-10-19 21:09:53 +0700

    new

--- hover-tabs/hover-tabs.js
+++ hover-tabs/hover-tabs.js
@@ -7,4 +7,10 @@ function sections(evt, Name) {
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].getElementsByClassName.display = "none";
     }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
 }
