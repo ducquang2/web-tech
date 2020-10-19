038452dc6492ca9af18635a3db6faed1eaec0187
ducquang2 <quangbilun@gmail.com>
2020-10-19 21:11:31 +0700

    done

--- hover-tabs/hover-tabs.js
+++ hover-tabs/hover-tabs.js
@@ -13,4 +13,8 @@ function sections(evt, Name) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
